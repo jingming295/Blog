fb5713c31c4fc57fa188e8159d4a1b12552f74d7
jingming295 <jingming295@gmail.com>
2024-01-13 03:09:58 +0800

    chore: improve code

--- server/src/routes/UploadArticle/performUploadArticle.ts
+++ server/src/routes/UploadArticle/performUploadArticle.ts
@@ -17,74 +17,63 @@ export class UploadArticle
     {
         this.returnData = new ReturnData();
     }
-    async performUploadArticle(data: { UserData: LD, ArticleData: AD })
-    {
-        try
-        {
+    async performUploadArticle(data: { UserData: LD, ArticleData: AD }) {
+        try {
             const decUserData: UD = this.decryptUserData(data.UserData.encUserData, data.UserData.userData.id.toString());
             const userData: UD = data.UserData.userData;
-            if (JSON.stringify(decUserData) === JSON.stringify(userData))
-            {
-                const articleData = data.ArticleData;
-                if(articleData.id && articleData.area && articleData.article && articleData.tag && articleData.title){
-                    const dbSelect = new DBSelect()
-                    const dbUpdate = new DBUpdate();
-
-                    const result = await dbSelect.selectArticleIDByItsAuthor(articleData.id, userData.id);
-                    if(result.length > 0){
-                        
-                        const ResultSetHeader = await dbUpdate.updateArticle(articleData.id, articleData.title, articleData.article, articleData.area, articleData.tag);
     
-                        if (!ResultSetHeader.warningStatus && ResultSetHeader.affectedRows)
-                        {
-                            const returnData = this.returnData.returnClientData(0, 'Update Sucessful');
-                            return returnData;
-                        } else
-                        {
-                            throw new Error(ResultSetHeader.info);
-                        }
-                    }else {
-                        const returnData = this.returnData.returnClientData(-101, 'You has no permission to edit this article');
-                        return returnData;
-                    }
-                } else if(articleData.area && articleData.article && articleData.tag && articleData.title){
-                    const dbInsert = new DBInsert();
-                    const ResultSetHeader = await dbInsert.postArticle(articleData.title, articleData.article, articleData.area, articleData.tag, userData.id);
+            if (JSON.stringify(decUserData) !== JSON.stringify(userData)) {
+                throw new Error('User data not match');
+            }
     
-                    if (!ResultSetHeader.warningStatus && ResultSetHeader.insertId)
-                    {
-                        const returnData = this.returnData.returnClientData(0, 'Post Sucessful');
-                        return returnData;
-                    } else
-                    {
-                        throw new Error(ResultSetHeader.info);
-                    }
-                } else {
-                    const returnData = this.returnData.returnClientData(-101, 'Missing input');
+            const articleData = data.ArticleData;
+    
+            if (!articleData.area || !articleData.article || !articleData.tag || !articleData.title) {
+                throw new Error('Missing input');
+            }
+    
+            const dbSelect = new DBSelect();
+            const dbUpdate = new DBUpdate();
+            const dbInsert = new DBInsert();
+    
+            if (articleData.id) {
+                const result = await dbSelect.selectArticleIDByItsAuthor(articleData.id, userData.id);
+    
+                if (result.length === 0) {
+                    const returnData = this.returnData.returnClientData(-101, 'You have no permission to edit this article');
                     return returnData;
                 }
-
-            } else
-            {
-                const returnData = this.returnData.returnClientData(-101, 'User data not match');
-                return returnData;
+    
+                const ResultSetHeader = await dbUpdate.updateArticle(articleData.id, articleData.title, articleData.article, articleData.area, articleData.tag);
+    
+                if (!ResultSetHeader.warningStatus && ResultSetHeader.affectedRows) {
+                    const returnData = this.returnData.returnClientData(0, 'Update Successful');
+                    return returnData;
+                } else {
+                    throw new Error(ResultSetHeader.info);
+                }
+            } else {
+                const ResultSetHeader = await dbInsert.postArticle(articleData.title, articleData.article, articleData.area, articleData.tag, userData.id);
+    
+                if (!ResultSetHeader.warningStatus && ResultSetHeader.insertId) {
+                    const returnData = this.returnData.returnClientData(0, 'Post Successful');
+                    return returnData;
+                } else {
+                    throw new Error(ResultSetHeader.info);
+                }
             }
-        } catch (error)
-        {
-            // invalid iv
-            if (error instanceof Error && error.message.includes('Unsupported state or unable to authenticate data'))
-            {
+        } catch (error) {
+            if (error instanceof Error && error.message.includes('Unsupported state or unable to authenticate data')) {
                 const returnData = this.returnData.returnClientData(-102, 'Invalid IV or Auth Tag');
                 return returnData;
-            } else
-            {
+            } else {
                 const returnData = this.returnData.returnClientData(-400, 'Error');
-                console.log(error);
+                console.error(error);
                 return returnData;
             }
         }
-
     }
+    
 
     decryptUserData(encUserData: AES_256_GCMEncrypted, key: string)
     {
